4d011ab1df6e0d232e64141ffadd119929fa938e
Roshan <kumarroshan77499@gmail.com>
2026-03-06 15:09:04 +0530

    Remove credentials and use env variables

--- dashboard/supabase_client.py
+++ dashboard/supabase_client.py
@@ -2,8 +2,8 @@ import streamlit as st
 from supabase import create_client
 import os
 
-url = "https://cwoajirwcejfdfntcqxn.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3b2FqaXJ3Y2VqZmRmbnRjcXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMDA1MTMsImV4cCI6MjA4NzU3NjUxM30.RQwYkl-QU0-m6e4c2gJ8lVfl815c-r2cdS5z5_CluzE"
+url = os.getenv("url")
+key = os.getenv("key")
 
 @st.cache_resource
 def get_supabase():
