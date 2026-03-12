9f17729072779211bb2da358ffa82dd1bc3406f4
Roshan <kumarroshan77499@gmail.com>
2026-03-12 20:21:46 +0530

    Initial commit - Mini Cloud Streamlit dashboard

--- dashboard/supabase_client.py
+++ dashboard/supabase_client.py
@@ -1,9 +1,17 @@
 import streamlit as st
 from supabase import create_client
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-url = os.getenv("url")
-key = os.getenv("key")
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+print("SUPABASE_URL:", url)
+print("SUPABASE_KEY:", key)
 
 @st.cache_resource
 def get_supabase():
